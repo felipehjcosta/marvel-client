e42bb2214b4d14a7a37c19c82c5d404d93790171
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-05-01 19:45:03 -0300

    Add simple API client

---
+++ src/androidMain/kotlin/ApplicationDispatcher.kt
@@ -0,0 +1,5 @@
+package com.github.felipehjcosta.marvelclient
+
+import kotlinx.coroutines.*
+
+internal actual val ApplicationDispatcher: CoroutineDispatcher = Dispatchers.Default
---
+++ src/commonMain/kotlin/ApplicationApi.kt
@@ -0,0 +1,27 @@
+package com.github.felipehjcosta.marvelclient
+
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.coroutines.*
+
+internal expect val ApplicationDispatcher: CoroutineDispatcher
+
+class ApplicationApi {
+    private val client = HttpClient()
+
+    private var address = Url("https://tools.ietf.org/rfc/rfc1866.txt")
+
+    fun about(callback: (String) -> Unit) {
+        GlobalScope.apply {
+            launch(ApplicationDispatcher) {
+                val result: String = client.get {
+                    url(this@ApplicationApi.address.toString())
+                }
+
+                callback(result)
+            }
+        }
+    }
+}
