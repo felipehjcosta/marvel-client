5869c268cafe7d218c6b116e56a197ef9578de49
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-05-12 11:00:10 -0300

    Append API result into browser page instead of logging then

--- library-js/src/main/kotlin/main.kt
+++ library-js/src/main/kotlin/main.kt
@@ -1,12 +1,16 @@
 import com.github.felipehjcosta.marvelclient.ApplicationApi
+import kotlin.browser.window
 
 val marvelPrivateKey: String = js("MARVEL_PRIVATE_KEY")
 val marvelPublicKey: String = js("MARVEL_PUBLIC_KEY")
 
 @JsName("libFunction")
 fun libFunction() {
-    ApplicationApi(marvelPrivateKey, marvelPublicKey).about {
-        println("about")
-        println(it)
-    }
+    ApplicationApi(marvelPrivateKey, marvelPublicKey).about(::showContent)
+}
+
+fun showContent(content: String) {
+    val helloWorld = window.document.createElement("p")
+    helloWorld.textContent = content
+    window.document.body?.appendChild(helloWorld)
 }
