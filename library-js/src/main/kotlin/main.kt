4fe1554efd3085d2b64a87b8bde28971afa6ee95
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-05-12 01:27:39 -0300

    Inject private and public key from gradle.properties into javascript

--- library-js/src/main/kotlin/main.kt
+++ library-js/src/main/kotlin/main.kt
@@ -1,9 +1,11 @@
 import com.github.felipehjcosta.marvelclient.ApplicationApi
 
+val marvelPrivateKey: String = js("MARVEL_PRIVATE_KEY")
+val marvelPublicKey: String = js("MARVEL_PUBLIC_KEY")
+
 @JsName("libFunction")
 fun libFunction() {
-    println("libFunction")
-    ApplicationApi().about {
+    ApplicationApi(marvelPrivateKey, marvelPublicKey).about {
         println("about")
         println(it)
     }
--- library-js/webpack.config.js
+++ library-js/webpack.config.js
@@ -1,19 +1,26 @@
 const path = require('path');
-
-module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: path.resolve(__dirname, 'build/classes/kotlin/main/library-js.js'),
-    resolve: {
-        "modules": [
-            path.resolve(__dirname, '/build/classes/kotlin/js/main'),
-            "node_modules"
+const webpack = require('webpack');
+module.exports = (env) => {
+    return {
+        mode: 'development',
+        devtool: 'source-map',
+        entry: path.resolve(__dirname, 'build/classes/kotlin/main/library-js.js'),
+        resolve: {
+            "modules": [
+                path.resolve(__dirname, '/build/classes/kotlin/js/main'),
+                "node_modules"
+            ]
+        },
+        output: {
+            filename: "kotlin-single-js-file-lib.js",
+            libraryTarget: "umd",
+            path: path.resolve(__dirname, 'build/webpack')
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'MARVEL_PRIVATE_KEY': JSON.stringify(env.MARVEL_PRIVATE_KEY),
+                'MARVEL_PUBLIC_KEY': JSON.stringify(env.MARVEL_PUBLIC_KEY)
+            })
         ]
-    },
-    output: {
-        filename: "kotlin-single-js-file-lib.js",
-        libraryTarget: "umd",
-        path: path.resolve(__dirname, 'build/webpack')
-    }
-
-};
+    };
+}
