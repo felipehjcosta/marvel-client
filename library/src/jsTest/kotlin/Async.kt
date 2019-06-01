b9240f428c7757fb8f01c0763bb90632ac46296e
Felipe Costa <felipe.h.j.costa@gmail.com>
2019-06-01 10:38:19 -0300

    Remove unused code

--- library/src/jsTest/kotlin/Async.kt
+++ library/src/jsTest/kotlin/Async.kt
@@ -1,5 +1,3 @@
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.promise
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
@@ -9,8 +7,6 @@ fun launch(block: suspend () -> Unit) {
     block.startCoroutine(object : Continuation<Unit> {
         override val context: CoroutineContext get() = EmptyCoroutineContext
         override fun resumeWith(result: Result<Unit>) {}
-//        override fun resume(value: Unit) {}
-//        override fun resumeWithException(e: Throwable) { console.log("Coroutine failed: $e") }
     })
 }
 
